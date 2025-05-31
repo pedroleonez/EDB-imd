347927b8862c5cb40e1503bffbe6cf020d3b5260
Pedro Vitor da Silva Melo Leonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-31 22:26:11 +0000

    feat: update Ex01 on aula05

--- lista_exercicios/aula05/Ex01.java
+++ lista_exercicios/aula05/Ex01.java
@@ -1,9 +1,17 @@
 package lista_exercicios.aula05;
 
+import java.util.Scanner;
+
 public class Ex01 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
         int vetor[] = {1, 2, 3, 4, 5};
-        int valorBuscado = 2;
+        
+        System.out.print("Digite o valor a ser buscado: ");
+        
+        int valorBuscado = sc.nextInt();
+        sc.close();
 
         for (int i = 0; i < vetor.length; i++) {
             if (vetor[i] == valorBuscado) {
